8b8862b28dc86c280f6e173c45e647d69047c719
Tommy Alex <iptux7@gmail.com>
2016-07-27 16:58:19 +0800

    get calling state from CallButtonPresenter.onStateChange
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

--- app/src/main/java/net/iptux/xposed/callrecording/ModCallRecording.java
+++ app/src/main/java/net/iptux/xposed/callrecording/ModCallRecording.java
@@ -8,6 +8,8 @@ import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
@@ -16,9 +18,18 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 
 	private static final String PACKAGE_DIALER = "com.android.dialer";
 	private static final String CALL_RECORDING_SERVICE = "com.android.services.callrecorder.CallRecorderService";
+	private static final String CALL_BUTTON_PRESENTER = "com.android.incallui.CallButtonPresenter";
 
 	private static final String PREF_RECORD_ENABLE = "record_enabled";
 	private static final String PREF_FORCE_AUDIO_SOURCE = "force_audio_source";
+
+	private static final String CALL_STATE_INCALL = "INCALL";
+	private static final String CALL_STATE_INCOMING = "INCOMING";
+	private static final String CALL_STATE_OUTGOING = "OUTGOING";
+
+	private static String sCallingState = "";
+	private static boolean sRecordIncoming = false;
+	private static boolean sRecordOutgoing = false;
 
 	@Override
 	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
@@ -67,6 +78,28 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 					param.setResult(trim);
 				}
 			});
+			final Class<?> CallButtonPresenter = XposedHelpers.findClass(CALL_BUTTON_PRESENTER, lpparam.classLoader);
+			XposedBridge.hookAllMethods(CallButtonPresenter, "onStateChange", new XC_MethodHook() {
+				@Override
+				protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+					updateCallState(param.args[0]);
+				}
+			});
 		}
+	}
+
+	void updateCallState(Object state) {
+		String newState = state.toString();
+		if (sCallingState.equals(newState))
+			return;
+
+		if (CALL_STATE_INCALL.equals(newState)) {
+			if (CALL_STATE_INCOMING.equals(sCallingState)) {
+				sRecordIncoming = true;
+			} else if (CALL_STATE_OUTGOING.equals(sCallingState)) {
+				sRecordOutgoing = true;
+			}
+		}
+		sCallingState = newState;
 	}
 }
