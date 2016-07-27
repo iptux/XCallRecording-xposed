7feb3103107a0c1aaaaec8fb8a066e92e0d28d84
Tommy Alex <iptux7@gmail.com>
2016-07-27 13:48:04 +0800

    handle force audio source setting
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

--- app/src/main/java/net/iptux/xposed/callrecording/ModCallRecording.java
+++ app/src/main/java/net/iptux/xposed/callrecording/ModCallRecording.java
@@ -1,6 +1,7 @@
 package net.iptux.xposed.callrecording;
 
 import android.content.Context;
+import android.media.MediaRecorder;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookInitPackageResources;
@@ -17,6 +18,7 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 	private static final String CALL_RECORDING_SERVICE = "com.android.services.callrecorder.CallRecorderService";
 
 	private static final String PREF_RECORD_ENABLE = "record_enabled";
+	private static final String PREF_FORCE_AUDIO_SOURCE = "force_audio_source";
 
 	@Override
 	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
@@ -24,6 +26,9 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 			XSharedPreferences prefs = new XSharedPreferences(PACKAGE_NAME);
 			if (prefs.getBoolean(PREF_RECORD_ENABLE, true)) {
 				resparam.res.setReplacement(PACKAGE_DIALER, "bool", "call_recording_enabled", true);
+			}
+			if (prefs.getBoolean(PREF_FORCE_AUDIO_SOURCE, false)) {
+				resparam.res.setReplacement(PACKAGE_DIALER, "integer", "call_recording_audio_source", MediaRecorder.AudioSource.VOICE_CALL);
 			}
 		}
 	}
@@ -33,12 +38,21 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 		if (PACKAGE_DIALER.equals(lpparam.packageName)) {
 			XSharedPreferences prefs = new XSharedPreferences(PACKAGE_NAME);
 			final boolean isEnabled = prefs.getBoolean(PREF_RECORD_ENABLE, true);
+			final boolean force_audio_source = prefs.getBoolean(PREF_FORCE_AUDIO_SOURCE, false);
 
 			findAndHookMethod(CALL_RECORDING_SERVICE, lpparam.classLoader, "isEnabled", Context.class, new XC_MethodHook() {
 				@Override
 				protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
 					if (isEnabled) {
 						param.setResult(Boolean.TRUE);
+					}
+				}
+			});
+			findAndHookMethod(CALL_RECORDING_SERVICE, lpparam.classLoader, "getAudioSource", new XC_MethodHook() {
+				@Override
+				protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+					if (force_audio_source) {
+						param.setResult(MediaRecorder.AudioSource.VOICE_CALL);
 					}
 				}
 			});
