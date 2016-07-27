387404a05427125143c205420b3acd456db9553a
Tommy Alex <iptux7@gmail.com>
2016-07-27 17:17:15 +0800

    start recording on needed
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

--- app/src/main/java/net/iptux/xposed/callrecording/ModCallRecording.java
+++ app/src/main/java/net/iptux/xposed/callrecording/ModCallRecording.java
@@ -2,8 +2,11 @@ package net.iptux.xposed.callrecording;
 
 import android.content.Context;
 import android.media.MediaRecorder;
+import android.os.Build;
+import android.view.View;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -19,9 +22,12 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 	private static final String PACKAGE_DIALER = "com.android.dialer";
 	private static final String CALL_RECORDING_SERVICE = "com.android.services.callrecorder.CallRecorderService";
 	private static final String CALL_BUTTON_PRESENTER = "com.android.incallui.CallButtonPresenter";
+	private static final String CALL_BUTTON_FRAGMENT = "com.android.incallui.CallButtonFragment";
 
 	private static final String PREF_RECORD_ENABLE = "record_enabled";
 	private static final String PREF_FORCE_AUDIO_SOURCE = "force_audio_source";
+	private static final String PREF_RECORD_INCOMING = "record_incoming";
+	private static final String PREF_RECORD_OUTGOING = "record_outgoing";
 
 	private static final String CALL_STATE_INCALL = "INCALL";
 	private static final String CALL_STATE_INCOMING = "INCOMING";
@@ -30,6 +36,7 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 	private static String sCallingState = "";
 	private static boolean sRecordIncoming = false;
 	private static boolean sRecordOutgoing = false;
+	private static String sRecordButtonFieldName = null;
 
 	@Override
 	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
@@ -55,6 +62,8 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 			}
 
 			final boolean force_audio_source = prefs.getBoolean(PREF_FORCE_AUDIO_SOURCE, false);
+			final boolean isRecordIncoming = prefs.getBoolean(PREF_RECORD_INCOMING, true);
+			final boolean isRecordOutgoing = prefs.getBoolean(PREF_RECORD_OUTGOING, true);
 
 			findAndHookMethod(CALL_RECORDING_SERVICE, lpparam.classLoader, "isEnabled", Context.class, new XC_MethodHook() {
 				@Override
@@ -85,6 +94,32 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 					updateCallState(param.args[0]);
 				}
 			});
+
+			if (Build.VERSION.SDK_INT >= 24) {
+				// not support yet
+			} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				sRecordButtonFieldName = "mCallRecordButton";
+			} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+				sRecordButtonFieldName = "mRecordButton";
+			} else {
+				// not support
+			}
+
+			final Class<?> CallButtonFragment = XposedHelpers.findClass(CALL_BUTTON_FRAGMENT, lpparam.classLoader);
+			// CallButtonFragment.setEnabled() is called frequently, start recording here is a good choice
+			XposedBridge.hookAllMethods(CallButtonFragment, "setEnabled", new XC_MethodHook() {
+				@Override
+				protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+					if (sRecordIncoming && isRecordIncoming) {
+						startRecordingByClickView(param.thisObject, sRecordButtonFieldName, 100);
+						sRecordIncoming = false;
+					}
+					if (sRecordOutgoing && isRecordOutgoing) {
+						startRecordingByClickView(param.thisObject, sRecordButtonFieldName, 100);
+						sRecordOutgoing = false;
+					}
+				}
+			});
 		}
 	}
 
@@ -101,5 +136,23 @@ public class ModCallRecording implements IXposedHookLoadPackage, IXposedHookInit
 			}
 		}
 		sCallingState = newState;
+	}
+
+	void startRecordingByClickView(Object obj, String name, long delayMillis) throws Throwable {
+		final View view = (View) getObjectField(obj, name);
+		if (null == view)
+			return;
+
+		view.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					view.performClick();
+				}
+				catch (Throwable e) {
+					XposedBridge.log(e);
+				}
+			}
+		}, delayMillis);
 	}
 }
